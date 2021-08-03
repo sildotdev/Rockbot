80aec98311b6cb74d51f8cf3574c371913433e9b
sil <git@sil.dev>
2021-08-03 17:46:59 -0500

    Ping command (using slash commands)

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -6,14 +6,24 @@ if (config.covid.enabled) require('./modules/covid.js')(client);
 
 client.on('ready', () => {
     console.log(`Rockbot logged in as ${client.user.tag}`)
+
+	client.api.applications(client.user.id).guilds('867553887186321419').commands.post({data: {
+		name: 'ping',
+		description: 'Ping pong!'
+	}})
 })
 
-client.on('message', msg => {
-	if (msg.content === 'nu!ping') {
-		msg.reply('Pong!');
-	}
-});
-
 client.login(config.token);
+
+client.ws.on('INTERACTION_CREATE', async interaction => {
+	if (interaction.data.name == 'ping') {
+		client.api.interactions(interaction.id, interaction.token).callback.post({data: {
+			type: 4,
+			data: {
+			  content: 'Pong!'
+			}
+		}})
+	}
+})
 
 module.exports = client
